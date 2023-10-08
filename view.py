a621b7dd211551371e8e0ca300145ae8ecd6aa21
Samara Lins <samara.e.lins@gmail.com>
2023-10-08 16:55:42 -0300

    problema da datat resolvido, formatação strftime

--- view.py
+++ view.py
@@ -4,5 +4,5 @@ from controller import*
 c1 = Categoria('Hortifruti','Frutas legumes e verduras, produtos da horta e do pomar')
 
 lista_compras = [{'Tomate':5},{'Leite':12},{'Shampoo':2}]
-venda = Venda(lista_compras,datetime.now().strfdatetime('%d/%m/%Y'),'Samara','Jefferson')
+venda = Venda(lista_compras,datetime.now().strftime('%d/%m/%Y'),'Samara','Jefferson')
 
